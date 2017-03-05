21bb1eee5632ee2efec55600c29016a100d0f294
Lex Myers <lex.myers@gmail.com>
2017-03-04 17:15:41 -0800

    added create route

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/v1/todos', (req, res, next) => {
+  const results = [];
+  const data = {text: req.body.text, complete: false};
+  pg.connect(connectionString, (err, client, done) => {
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    client.query('INSERT INTO items(text, complete) values($1, $2)',
+    [data.text, data.complete]);
+    const query = client.query('SELECT * FROM items ORDER BY id ASC');
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
