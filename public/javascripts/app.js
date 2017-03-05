6aeb7e74a58a20554b95511cc4396e447fbf93c2
Lex Myers <lex.myers@gmail.com>
2017-03-04 23:26:58 -0800

    added create and delete functions to angular module

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,4 +10,25 @@ angular.module('nodeTodo', [])
   .error((error) => {
     console.log('Error: ' + error);
   });
-})
+  $scope.createTodo = () => {
+    $http.post('/api/v1/todos', $scope.formData)
+    .success((data) => {
+      $scope.formData = {};
+      $scope.todoData = data;
+      console.log(data);
+    })
+    .error((error) => {
+      console.log('Error: ' + error);
+    });
+  };
+  $scope.deleteTodo = (totoID) => {
+    $http.delete('/api/v1/todos' + todoID)
+    .success((data) => {
+      $scope.todoData = data;
+      console.log(data);
+    })
+    .error((data) => {
+      console.log('Error: ' + data);
+    });
+  };
+});
